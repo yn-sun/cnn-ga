06e90f57b98a3aa39ccaa52727de7bf7296e102c
benchenas <97876328+benchenas@users.noreply.github.com>
2022-04-25 21:22:54 +0800

    Update cifar10.py

--- template/cifar10.py
+++ template/cifar10.py
@@ -51,8 +51,9 @@ class EvoCNNModel(nn.Module):
 
 class TrainModel(object):
     def __init__(self):
-        trainloader, validate_loader = data_loader.get_train_valid_loader('/home/yanan/train_data', batch_size=128, augment=True, valid_size=0.1, shuffle=True, random_seed=2312390, show_sample=False, num_workers=1, pin_memory=True)
-        #testloader = data_loader.get_test_loader('/home/yanan/train_data', batch_size=128, shuffle=False, num_workers=1, pin_memory=True)
+        data_dir = os.path.expanduser('./dataset')
+        trainloader, validate_loader = data_loader.get_train_valid_loader(data_dir, batch_size=128, augment=True, valid_size=0.1, shuffle=True, random_seed=2312390, show_sample=False, num_workers=1, pin_memory=True)
+        #testloader = data_loader.get_test_loader(data_dir, batch_size=128, shuffle=False, num_workers=1, pin_memory=True)
         net = EvoCNNModel()
         cudnn.benchmark = True
         net = net.cuda()
@@ -98,7 +99,7 @@ class TrainModel(object):
             loss = self.criterion(outputs, labels)
             loss.backward()
             optimizer.step()
-            running_loss += loss.data[0]*labels.size(0)
+            running_loss += loss.item()*labels.size(0)
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             correct += (predicted == labels.data).sum()
@@ -114,7 +115,7 @@ class TrainModel(object):
             inputs, labels = Variable(inputs.cuda()), Variable(labels.cuda())
             outputs = self.net(inputs)
             loss = self.criterion(outputs, labels)
-            test_loss += loss.data[0]*labels.size(0)
+            test_loss += loss.item()*labels.size(0)
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             correct += (predicted == labels.data).sum()
@@ -136,8 +137,8 @@ class RunModel(object):
     def do_work(self, gpu_id, file_id):
         os.environ['CUDA_VISIBLE_DEVICES'] = gpu_id
         best_acc = 0.0
+        m = TrainModel()
         try:
-            m = TrainModel()
             m.log_record('Used GPU#%s, worker name:%s[%d]'%(gpu_id, multiprocessing.current_process().name, os.getpid()), first_time=True)
             best_acc = m.process()
             #import random
